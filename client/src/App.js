27116871b8fd60f3c8862c99856483d521cc5848
ZQWhiting <zach.whiting@icloud.com>
2020-09-22 17:22:22 -0600

    added react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
@@ -7,6 +8,11 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 const client = new ApolloClient({
 	uri: '/graphql',
@@ -15,13 +21,27 @@ const client = new ApolloClient({
 function App() {
 	return (
 		<ApolloProvider client={client}>
-			<div className='flex-column justify-flex-start min-100-vh'>
-				<Header />
-				<div className='container'>
-					<Home />
+			<Router>
+				<div className='flex-column justify-flex-start min-100-vh'>
+					<Header />
+					<div className='container'>
+						<Switch>
+							<Route exact path='/' component={Home} />
+							<Route exact path='/login' component={Login} />
+							<Route exact path='/signup' component={Signup} />
+							<Route exact path='/profile' component={Profile} />
+							<Route
+								exact
+								path='/thought'
+								component={SingleThought}
+							/>
+
+							<Route component={NoMatch} />
+						</Switch>
+					</div>
+					<Footer />
 				</div>
-				<Footer />
-			</div>
+			</Router>
 		</ApolloProvider>
 	);
 }
