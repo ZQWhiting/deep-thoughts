fff224cef6b5b6114526abda6d1a1eb2c8e2e1d6
ZQWhiting <zach.whiting@icloud.com>
2020-09-22 16:21:20 -0600

    set up Apollo Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+const client = new ApolloClient({
+	uri: 'http://localhost:3001/graphql',
+});
+
 function App() {
-  return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
-      </div>
-      <Footer />
-    </div>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<div className='flex-column justify-flex-start min-100-vh'>
+				<Header />
+				<div className='container'>
+					<Home />
+				</div>
+				<Footer />
+			</div>
+		</ApolloProvider>
+	);
 }
 
 export default App;
