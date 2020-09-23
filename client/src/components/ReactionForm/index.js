0d78f8a514c69390becb8afa68ff0e9a378ebcd3
ZQWhiting <zach.whiting@icloud.com>
2020-09-22 21:43:52 -0600

    created ReactionForm component

---
+++ client/src/components/ReactionForm/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+const ReactionForm = ({ thoughtId }) => {
+	const [reactionBody, setBody] = useState('');
+	const [characterCount, setCharacterCount] = useState(0);
+
+	const handleChange = (event) => {
+		if (event.target.value.length <= 280) {
+			setBody(event.target.value);
+			setCharacterCount(event.target.value.length);
+		}
+	};
+
+	const handleFormSubmit = async (event) => {
+		event.preventDefault();
+
+		setBody('');
+		setCharacterCount(0);
+	};
+
+	return (
+		<div>
+			<p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
+				Character Count: {characterCount}/280
+			</p>
+			<form
+				className='flex-row justify-center justify-space-between-md align-stretch'
+				onSubmit={handleFormSubmit}
+			>
+				<textarea
+					placeholder='Leave a reaction to this thought...'
+					value={reactionBody}
+					className='form-input col-12 col-md-9'
+					onChange={handleChange}
+				></textarea>
+				<button className='btn col-12 col-md-3' type='submit'>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
+};
+
+export default ReactionForm;
--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -3,8 +3,10 @@ import { useParams } from 'react-router-dom';
 
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_THOUGHT } from '../utils/queries';
+import Auth from '../utils/auth';
 
 import ReactionList from '../components/ReactionList'
+import ReactionForm from '../components/ReactionForm';
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams();
@@ -20,21 +22,24 @@ const SingleThought = props => {
   }
 
   return (
-    <div>
-      <div className="card mb-3">
-        <p className="card-header">
-          <span style={{ fontWeight: 700 }} className="text-light">
-            {thought.username}
-          </span>{' '}
-          thought on {thought.createdAt}
-        </p>
-        <div className="card-body">
-          <p>{thought.thoughtText}</p>
-        </div>
-      </div>
+		<div>
+			<div className='card mb-3'>
+				<p className='card-header'>
+					<span style={{ fontWeight: 700 }} className='text-light'>
+						{thought.username}
+					</span>{' '}
+					thought on {thought.createdAt}
+				</p>
+				<div className='card-body'>
+					<p>{thought.thoughtText}</p>
+				</div>
+			</div>
 
-      {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
-    </div>
+			{thought.reactionCount > 0 && (
+				<ReactionList reactions={thought.reactions} />
+			)}
+			{Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
+		</div>
   );
 };
 
