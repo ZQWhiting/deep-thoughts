6cca0192e9c78207f745940b9286a0d59b8d33f5
ZQWhiting <zach.whiting@icloud.com>
2020-09-22 18:20:27 -0600

    login page functionality

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 
+import { useMutation } from '@apollo/react-hooks';
+import { LOGIN_USER } from '../utils/mutations';
+
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -17,6 +21,16 @@ const Login = (props) => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
+    try {
+		const { data } = await login({
+			variables: { ...formState },
+		});
+
+		console.log(data);
+	} catch (e) {
+		console.error(e);
+	}
+
     // clear form values
     setFormState({
       email: '',
@@ -25,38 +39,39 @@ const Login = (props) => {
   };
 
   return (
-    <main className='flex-row justify-center mb-4'>
-      <div className='col-12 col-md-6'>
-        <div className='card'>
-          <h4 className='card-header'>Login</h4>
-          <div className='card-body'>
-            <form onSubmit={handleFormSubmit}>
-              <input
-                className='form-input'
-                placeholder='Your email'
-                name='email'
-                type='email'
-                id='email'
-                value={formState.email}
-                onChange={handleChange}
-              />
-              <input
-                className='form-input'
-                placeholder='******'
-                name='password'
-                type='password'
-                id='password'
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button className='btn d-block w-100' type='submit'>
-                Submit
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </main>
+		<main className='flex-row justify-center mb-4'>
+			<div className='col-12 col-md-6'>
+				<div className='card'>
+					<h4 className='card-header'>Login</h4>
+					<div className='card-body'>
+						<form onSubmit={handleFormSubmit}>
+							<input
+								className='form-input'
+								placeholder='Your email'
+								name='email'
+								type='email'
+								id='email'
+								value={formState.email}
+								onChange={handleChange}
+							/>
+							<input
+								className='form-input'
+								placeholder='******'
+								name='password'
+								type='password'
+								id='password'
+								value={formState.password}
+								onChange={handleChange}
+							/>
+							<button className='btn d-block w-100' type='submit'>
+								Submit
+							</button>
+						</form>
+						{error && <div>Login failed</div>}
+					</div>
+				</div>
+			</div>
+		</main>
   );
 };
 
