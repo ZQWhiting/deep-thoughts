cece3e5c9490eed29b1d584273df556b5cae13db
ZQWhiting <zach.whiting@icloud.com>
2020-09-22 18:31:19 -0600

    created AuthService class for authentication

---
+++ client/src/utils/auth.js
@@ -0,0 +1,47 @@
+import decode from 'jwt-decode';
+
+class AuthService {
+	// retrieve data saved in token
+	getProfile() {
+		return decode(this.getToken());
+	}
+
+	// check if the user is still logged in
+	loggedIn() {
+		// checks if there is a saved token and it's still valid
+		const token = this.getToken();
+		// use type coersion to check if token is NOT undefined and the token is NOT expired
+		return !!token && !this.isTokenExpired(token);
+	}
+
+	// check if the token has expired
+	isTokenExpired(token) {
+		try {
+			const decoded = decode(token);
+			!decoded.exp < Date.now() / 1000 ? true : false;
+		} catch (err) {
+			return false;
+		}
+	}
+
+	// retrieve token from localStorage
+	getToken() {
+		return localStorage.getItem('id_token');
+	}
+
+	// set token to localStorage and reload page to homepage
+	login(idToken) {
+		localStorage.setItem('id_token', idToken);
+
+		window.location.assign('/');
+	}
+
+	// clear token from localStorage and force logout with reload
+	logout() {
+		localStorage.removeItem('id_token');
+
+		window.location.assign('/');
+	}
+}
+
+export default new AuthService();
