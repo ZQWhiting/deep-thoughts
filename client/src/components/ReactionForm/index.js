d5e9d2e2aa009ff915351e79c26b3328905888d7
ZQWhiting <zach.whiting@icloud.com>
2020-09-22 21:49:29 -0600

    ReactionForm functionality

--- client/src/components/ReactionForm/index.js
+++ client/src/components/ReactionForm/index.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 
+import { ADD_REACTION } from '../../utils/mutations';
+import { useMutation } from '@apollo/react-hooks';
+
 const ReactionForm = ({ thoughtId }) => {
 	const [reactionBody, setBody] = useState('');
-	const [characterCount, setCharacterCount] = useState(0);
+    const [characterCount, setCharacterCount] = useState(0);
+
+    const [addReaction, { error }] = useMutation(ADD_REACTION);
 
 	const handleChange = (event) => {
 		if (event.target.value.length <= 280) {
@@ -12,16 +17,27 @@ const ReactionForm = ({ thoughtId }) => {
 	};
 
 	const handleFormSubmit = async (event) => {
-		event.preventDefault();
+        event.preventDefault();
 
-		setBody('');
-		setCharacterCount(0);
+        try {
+			await addReaction({
+				variables: { reactionBody, thoughtId },
+			});
+
+			setBody('');
+		    setCharacterCount(0);
+		} catch (e) {
+			console.error(e);
+		}
+
+
 	};
 
 	return (
 		<div>
 			<p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
 				Character Count: {characterCount}/280
+				{error && <span className='ml-2'>Something went wrong...</span>}
 			</p>
 			<form
 				className='flex-row justify-center justify-space-between-md align-stretch'
