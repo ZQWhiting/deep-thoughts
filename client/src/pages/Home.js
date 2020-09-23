218c1a3e27cc0b67f0da5eae1fb4d7758aff8b92
ZQWhiting <zach.whiting@icloud.com>
2020-09-22 20:34:35 -0600

    added FriendList to homepage for loggedIn users

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,25 @@
 import React from 'react';
 
 import { useQuery } from '@apollo/react-hooks';
-import { QUERY_THOUGHTS } from '../utils/queries';
+import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
+
+import Auth from '../utils/auth';
 
 import ThoughtList from '../components/ThoughtList';
+import FriendList from '../components/FriendList';
 
 const Home = () => {
+	const { loading, data } = useQuery(QUERY_THOUGHTS);
+	const { data: userData } = useQuery(QUERY_ME_BASIC);
 
-  const { loading, data } = useQuery(QUERY_THOUGHTS);
-  const thoughts = data?.thoughts || [];
+	const thoughts = data?.thoughts || [];
 
-  return (
+	const loggedIn = Auth.loggedIn();
+
+	return (
 		<main>
 			<div className='flex-row justify-space-between'>
-				<div className='col-12 mb-3'>
+				<div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
 					{loading ? (
 						<div>Loading...</div>
 					) : (
@@ -23,9 +29,18 @@ const Home = () => {
 						/>
 					)}
 				</div>
+				{loggedIn && userData ? (
+					<div className='col-12 col-lg-3 mb-3'>
+						<FriendList
+							username={userData.me.username}
+							friendCount={userData.me.friendCount}
+							friends={userData.me.friends}
+						/>
+					</div>
+				) : null}
 			</div>
 		</main>
-  );
+	);
 };
 
 export default Home;
