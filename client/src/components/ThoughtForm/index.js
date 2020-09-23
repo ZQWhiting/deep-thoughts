d945f22e4eb87c96196fe54591c70b52f739f80f
ZQWhiting <zach.whiting@icloud.com>
2020-09-22 21:34:05 -0600

    functionality for ThoughtForm component

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,8 +1,37 @@
 import React, { useState } from 'react';
 
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_THOUGHT } from '../../utils/mutations';
+import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
+
 const ThoughtForm = () => {
 	const [thoughtText, setText] = useState('');
 	const [characterCount, setCharacterCount] = useState(0);
+
+	const [addThought, { error }] = useMutation(ADD_THOUGHT, {
+		update(cache, { data: { addThought } }) {
+			// QUERY_THOUGHTS could potentially not be cached yet
+			try {
+				// read what's currently in the cache
+				const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+
+				// prepend the newest thought to the front of the array
+				cache.writeQuery({
+					query: QUERY_THOUGHTS,
+					data: { thoughts: [addThought, ...thoughts] },
+				});
+			} catch (e) {
+				console.error(e);
+			}
+
+			// do the same for QUERY_ME cache
+			const { me } = cache.readQuery({ query: QUERY_ME });
+			cache.writeQuery({
+				query: QUERY_ME,
+				data: { me: { ...me, thoughts: [...me.thoughts, addThought] } },
+			});
+		},
+	});
 
 	const handleChange = (event) => {
 		if (event.target.value.length <= 280) {
@@ -13,14 +42,24 @@ const ThoughtForm = () => {
 
 	const handleFormSubmit = async (event) => {
 		event.preventDefault();
-		setText('');
-		setCharacterCount(0);
+
+		try {
+			await addThought({
+				variables: { thoughtText },
+			});
+
+			setText('');
+			setCharacterCount(0);
+		} catch (e) {
+			console.error(e);
+		}
 	};
 
 	return (
 		<div>
 			<p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
 				Character Count: {characterCount}/280
+				{error && <span className='ml-2'>Something went wrong...</span>}
 			</p>
 			<form
 				className='flex-row justify-center justify-space-between-md align-stretch'
