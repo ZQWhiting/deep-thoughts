bfffea6639b7079b321fb04dc976d3a015b8ef40
ZQWhiting <zach.whiting@icloud.com>
2020-09-22 20:50:43 -0600

    customized Profile page for loggedIn users

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,31 +1,50 @@
 import React from 'react';
 
-import { useParams } from 'react-router-dom';
+import { useParams, Redirect } from 'react-router-dom';
 
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 
 import { useQuery } from '@apollo/react-hooks';
-import { QUERY_USER } from '../utils/queries';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+
+import Auth from '../utils/auth';
 
 const Profile = () => {
 	const { username: userParam } = useParams();
 
-	const { loading, data } = useQuery(QUERY_USER, {
+	const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
 		variables: { username: userParam },
 	});
 
-	const user = data?.user || {};
+	const user = data?.me || data?.user || {};
+
+	if (
+		Auth.loggedIn() &&
+		Auth.getProfile().data.username.toLowerCase() ===
+			userParam?.toLowerCase()
+	) {
+		return <Redirect to='/profile' />;
+	}
 
 	if (loading) {
 		return <div>Loading...</div>;
+	}
+
+	if (!user?.username) {
+		return (
+			<h4>
+				You need to be logged in to see this page. Use the navigation
+				links above to sign up or log in!
+			</h4>
+		);
 	}
 
 	return (
 		<div>
 			<div className='flex-row mb-3'>
 				<h2 className='bg-dark text-secondary p-3 display-inline-block'>
-					Viewing {user.username}'s profile.
+					Viewing {userParam ? `${user.username}'s` : 'your'} profile.
 				</h2>
 			</div>
 
@@ -47,6 +66,6 @@ const Profile = () => {
 			</div>
 		</div>
 	);
-};
+};;
 
 export default Profile;
