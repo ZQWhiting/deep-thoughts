f05061584268299966a2d2d28ec4fc1c30029c2c
ZQWhiting <zach.whiting@icloud.com>
2020-09-22 20:16:31 -0600

    added logout to navbar

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth'
 
 const Header = () => {
-	console.log(Auth.loggedIn())
+	const logout = (event) => {
+		event.preventDefault();
+		Auth.logout();
+	};
+
 	return (
 		<header className='bg-secondary mb-4 py-2 flex-row align-center'>
 			<div className='container flex-row justify-space-between-lg justify-center align-center'>
@@ -16,7 +20,7 @@ const Header = () => {
 					{Auth.loggedIn() ? (
 						<>
 							<Link to='/profile'>Me</Link>
-							<a href='/'>
+							<a href='/' onClick={logout}>
 								Logout
 							</a>
 						</>
